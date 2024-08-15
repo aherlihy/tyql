91d91673f573a40a1b8247cee9c379c92ac0d7e7
aherlihy <herlihyap@gmail.com>
2024-08-15 12:27:54 +0200

    finish expected compile error tests

--- src/test/scala/test/query/FailTests.scala
+++ src/test/scala/test/query/FailTests.scala
@@ -1,17 +1,11 @@
 package test.query.fail
-//
-//import test.{SQLStringAggregationTest, SQLStringQueryTest}
-//import tyql.Expr.sum
-//
-//import java.time.LocalDate
-// TODO: tests that should fail to compile
-//
-class TestCompileError extends munit.FunSuite {
+
+class MapMapCompileErrorTest extends munit.FunSuite {
   def testDescription: String = "map+map should fail with useful error message"
   def expectedError: String = "Cannot return an Query from a map. Did you mean to use flatMap?"
 
   test(testDescription) {
-    assert(
+    val error: String =
       compileErrors(
         """
            // BOILERPLATE
@@ -40,128 +34,359 @@ class TestCompileError extends munit.FunSuite {
              purchases = Table[Purchase]("purchase")
            )
 
-
+           // TEST
            tables.buyers.map(b =>
              tables.shipInfos.map(si =>
-               (name = b.name, shippingDate = si.shippingDate).toRow
+               (name = b.name, shippingDate = si.shippingDate)
              )
            )
-          """).contains(expectedError)
-    )
+        """)
+    assert(error.contains(expectedError), s"Expected substring '$expectedError' in '$error'")
   }
 }
-//class FlowMapTest extends SQLStringTest[AllCommerceDBs, (name: String, shippingDate: LocalDate)] {
-//  def testDescription = "Flow: project tuple, 2 nest,"
-//  def query() =
-//    testDB.tables.buyers.map(b =>
-//      testDB.tables.shipInfos.map(si =>
-//        (name = b.name, shippingDate = si.shippingDate).toRow
-//      )
-//    )
-//
-//  def sqlString = "SELECT r0.name, r1.shippingDate FROM buyers r0, shipInfos r1"
-//}
+class MapMapTRCompileErrorTest extends munit.FunSuite {
+  def testDescription: String = "map+map with toRow should fail with useful error message"
+  def expectedError: String = "Cannot return an Query from a map. Did you mean to use flatMap?"
 
-//class FlowMapAggregate3Test extends SQLStringTest[AllCommerceDBs, Int] {
-//  def testDescription = "Flow: project tuple, 2 nest, map+map should fail even with aggregation"
-//  def query() =
-//    testDB.tables.buyers.map(b =>
-//      testDB.tables.shipInfos.map(si =>
-//        si.buyerId.sum
-//      )
-//    )
-//
-//  def sqlString = "SELECT MAX(r1.buyerId) FROM buyers r0, shipInfos r1"
-//}
+  test(testDescription) {
+    val error: String =
+      compileErrors(
+        """
+           // BOILERPLATE
+           import language.experimental.namedTuples
+           import tyql.Table
+           import java.time.LocalDate
 
-//class AggregationSum3SelectTest extends SQLStringTest[AllCommerceDBs, Double] {
-//  def testDescription: String = "Aggregation: sum with map"
-//  def query() =
-//    testDB.tables.products.withFilter(p => p.price != 0).sum(_.price).map(_) // should fail to compile because cannot call query methods on aggregation result
-//  def sqlString: String = "SELECT SUM(product.price) FROM product WHERE product.price > 0"
-//}
+           case class Product(id: Int, name: String, price: Double)
 
-//class OuterFlatMapTest extends SQLStringTest[AllCommerceDBs, (bName: String, bId: Int)] {
-//  def testDescription = "Flow: project tuple, no nest, flatMap"
-//
-//  def query() =
-//    testDB.tables.buyers.flatMap(b =>
-//      (bName = b.name, bId = b.id).toRow
-//    )
-//    ???
-//  def sqlString = "SELECT r0.name, r0.id FROM buyers r0 WHERE r0.id > 0"
-//}
-//class OuterFlatMapNestedFlatMapTest extends SQLStringTest[AllCommerceDBs, (name: String, shippingDate: LocalDate)] {
-//  def testDescription = "Flow: project tuple, 2 nest, flatmap+flatmap"
-//
-//  def query() =
-//    testDB.tables.buyers.flatMap(b =>
-//      testDB.tables.shipInfos.flatMap(si =>
-//        (name = b.name, shippingDate = si.shippingDate).toRow
-//      )
-//    )
-//    ???
-//
-//  def sqlString = "SELECT r0.name, r1.shippingDate FROM buyers r0, shipInfos r1"
-//}
-//
-//class OuterMapNestedMapTest extends SQLStringTest[AllCommerceDBs, (name: String, shippingDate: LocalDate)] {
-//  def testDescription = "Flow: project tuple, 2 nest, map+map"
-//
-//  def query() =
-//    testDB.tables.buyers.map(b =>
-//      testDB.tables.shipInfos.map(si =>
-//        (name = b.name, shippingDate = si.shippingDate).toRow
-//      )
-//    )
-//    ???
-//
-//  def sqlString = "SELECT r0.name, r1.shippingDate FROM buyers r0, shipInfos r1"
-//}
-//class OuterMapNestedFlatMapTest extends SQLStringTest[AllCommerceDBs, (name: String, shippingDate: LocalDate)] {
-//  def testDescription = "Flow: project tuple, 2 nest, map+flatmap"
-//
-//  def query() =
-//    testDB.tables.buyers.map(b =>
-//      testDB.tables.shipInfos.flatMap(si =>
-//        (name = b.name, shippingDate = si.shippingDate).toRow
-//      )
-//    )
-//    ???
-//
-//  def sqlString = "SELECT r0.name, r1.shippingDate FROM buyers r0, shipInfos r1"
-//}
+           case class Buyer(id: Int, name: String, dateOfBirth: LocalDate)
 
-//class AggregateScalarFailTest extends SQLStringAggregationTest[AllCommerceDBs, Double] {
-//  def testDescription: String = "Aggregation: aggregate + scalar expr should fail"
-//
-//  def query() =
-//    testDB.tables.products
-//      .aggregate(p => p.price)
-//
-//  def sqlString: String = "SELECT SUM(purchase.price) FROM purchase"
-//}
-//class AggregationSubqueryFailTest extends SQLStringQueryTest[AllCommerceDBs, Boolean] {
-//  def testDescription: String = "Aggregation: aggregate with aggregate subquery as source (should fail)"
-//
-//  def query() =
-//    testDB.tables.products.aggregate(p => (avgPrice = p.price.avg)).map(r => r == 10)
-//
-//  def sqlString: String =
-//    """
-//          SELECT avgPrice = 10
-//          FROM (SELECT AVG(price) as avgPrice FROM products)
-//        """
-//}
-//class FlowMapAggregateTestFail extends SQLStringAggregationTest[AllCommerceDBs, Int] {
-//  def testDescription = "Flow: 2 nest, map+map should fail even with aggregate"
-//
-//  def query() =
-//    testDB.tables.buyers.map(b =>
-//      testDB.tables.shipInfos.map(si =>
-//        sum(si.buyerId)
-//      )
-//    )
-//
-//  def sqlString = ""
-//}
+           case class ShippingInfo(id: Int, buyerId: Int, shippingDate: LocalDate)
+
+           case class Purchase(
+                     id: Int,
+                     shippingInfoId: Int,
+                     productId: Int,
+                     count: Int,
+                     total: Double
+                   )
+
+           val tables = (
+             products = Table[Product]("product"),
+             buyers = Table[Buyer]("buyers"),
+             shipInfos = Table[ShippingInfo]("shippingInfo"),
+             purchases = Table[Purchase]("purchase")
+           )
+
+          // TEST
+          tables.buyers.map(b =>
+            tables.shipInfos.map(si =>
+              (name = b.name, shippingDate = si.shippingDate).toRow
+            )
+          )
+          """)
+    assert(error.contains(expectedError), s"Expected substring '$expectedError' in '$error'")
+  }
+}
+
+class MapMapAggregateCompileErrorTest extends munit.FunSuite {
+  def testDescription: String = "map+map with aggregate should fail with useful error message"
+  def expectedError: String = "Cannot return an Query from a map. Did you mean to use flatMap?"
+
+  test(testDescription) {
+    val error: String =
+      compileErrors(
+        """
+           // BOILERPLATE
+           import language.experimental.namedTuples
+           import tyql.{Table, Expr}
+           import java.time.LocalDate
+
+           case class Product(id: Int, name: String, price: Double)
+
+           case class Buyer(id: Int, name: String, dateOfBirth: LocalDate)
+
+           case class ShippingInfo(id: Int, buyerId: Int, shippingDate: LocalDate)
+
+           case class Purchase(
+                     id: Int,
+                     shippingInfoId: Int,
+                     productId: Int,
+                     count: Int,
+                     total: Double
+                   )
+
+           val tables = (
+             products = Table[Product]("product"),
+             buyers = Table[Buyer]("buyers"),
+             shipInfos = Table[ShippingInfo]("shippingInfo"),
+             purchases = Table[Purchase]("purchase")
+           )
+
+          // TEST
+          tables.buyers.map(b =>
+            tables.shipInfos.map(si =>
+              Expr.sum(si.buyerId)
+            )
+          )
+          """)
+    assert(error.contains(expectedError), s"Expected substring '$expectedError' in '$error'")
+  }
+}
+
+class MapAfterAggregateCompileErrorTest extends munit.FunSuite {
+  def testDescription: String = "map after aggregate should fail with useful error message"
+  def expectedError: String = "value map is not a member of tyql.Aggregation"
+
+  test(testDescription) {
+    val error: String =
+      compileErrors(
+        """
+           // BOILERPLATE
+           import language.experimental.namedTuples
+           import tyql.{Table, Expr}
+           import java.time.LocalDate
+
+           case class Product(id: Int, name: String, price: Double)
+
+           case class Buyer(id: Int, name: String, dateOfBirth: LocalDate)
+
+           case class ShippingInfo(id: Int, buyerId: Int, shippingDate: LocalDate)
+
+           case class Purchase(
+                     id: Int,
+                     shippingInfoId: Int,
+                     productId: Int,
+                     count: Int,
+                     total: Double
+                   )
+
+           val tables = (
+             products = Table[Product]("product"),
+             buyers = Table[Buyer]("buyers"),
+             shipInfos = Table[ShippingInfo]("shippingInfo"),
+             purchases = Table[Purchase]("purchase")
+           )
+
+          // TEST
+          tables.products.withFilter(p => p.price != 0).sum(_.price).map(_) // should fail to compile because cannot call query methods on aggregation result
+          """)
+    assert(error.contains(expectedError), s"Expected substring '$expectedError' in '$error'")
+  }
+}
+
+class FlatmapOnlyCompileErrorTest extends munit.FunSuite {
+  def testDescription: String = "flatMap without inner map fails"
+  def expectedError: String = "Cannot return an Expr from a flatMap. Did you mean to use map?"
+
+  test(testDescription) {
+    val error: String =
+      compileErrors(
+        """
+           // BOILERPLATE
+           import language.experimental.namedTuples
+           import tyql.{Table, Expr}
+           import java.time.LocalDate
+
+           case class Product(id: Int, name: String, price: Double)
+
+           case class Buyer(id: Int, name: String, dateOfBirth: LocalDate)
+
+           case class ShippingInfo(id: Int, buyerId: Int, shippingDate: LocalDate)
+
+           case class Purchase(
+                     id: Int,
+                     shippingInfoId: Int,
+                     productId: Int,
+                     count: Int,
+                     total: Double
+                   )
+
+           val tables = (
+             products = Table[Product]("product"),
+             buyers = Table[Buyer]("buyers"),
+             shipInfos = Table[ShippingInfo]("shippingInfo"),
+             purchases = Table[Purchase]("purchase")
+           )
+
+          // TEST
+          tables.buyers.flatMap(b =>
+            (bName = b.name, bId = b.id).toRow
+          )
+          """)
+    assert(error.contains(expectedError), s"Expected substring '$expectedError' in '$error'")
+  }
+}
+
+class FlatmapFlatmapCompileErrorTest extends munit.FunSuite {
+  def testDescription: String = "flatMap without inner map fails"
+  def expectedError: String = "Cannot return an Expr from a flatMap. Did you mean to use map?"
+
+  test(testDescription) {
+    val error: String =
+      compileErrors(
+        """
+           // BOILERPLATE
+           import language.experimental.namedTuples
+           import tyql.{Table, Expr}
+           import java.time.LocalDate
+
+           case class Product(id: Int, name: String, price: Double)
+
+           case class Buyer(id: Int, name: String, dateOfBirth: LocalDate)
+
+           case class ShippingInfo(id: Int, buyerId: Int, shippingDate: LocalDate)
+
+           case class Purchase(
+                     id: Int,
+                     shippingInfoId: Int,
+                     productId: Int,
+                     count: Int,
+                     total: Double
+                   )
+
+           val tables = (
+             products = Table[Product]("product"),
+             buyers = Table[Buyer]("buyers"),
+             shipInfos = Table[ShippingInfo]("shippingInfo"),
+             purchases = Table[Purchase]("purchase")
+           )
+
+          // TEST
+          tables.buyers.flatMap(b =>
+            tables.shipInfos.flatMap(si =>
+              (name = b.name, shippingDate = si.shippingDate).toRow
+            )
+          )
+          """)
+    assert(error.contains(expectedError), s"Expected substring '$expectedError' in '$error'")
+  }
+}
+
+class MapFlatmapCompileErrorTest extends munit.FunSuite {
+  def testDescription: String = "map with inner flatMap fails"
+  def expectedError: String = "Cannot return an Expr from a flatMap. Did you mean to use map?"
+
+  test(testDescription) {
+    val error: String =
+      compileErrors(
+        """
+           // BOILERPLATE
+           import language.experimental.namedTuples
+           import tyql.{Table, Expr}
+           import java.time.LocalDate
+
+           case class Product(id: Int, name: String, price: Double)
+
+           case class Buyer(id: Int, name: String, dateOfBirth: LocalDate)
+
+           case class ShippingInfo(id: Int, buyerId: Int, shippingDate: LocalDate)
+
+           case class Purchase(
+                     id: Int,
+                     shippingInfoId: Int,
+                     productId: Int,
+                     count: Int,
+                     total: Double
+                   )
+
+           val tables = (
+             products = Table[Product]("product"),
+             buyers = Table[Buyer]("buyers"),
+             shipInfos = Table[ShippingInfo]("shippingInfo"),
+             purchases = Table[Purchase]("purchase")
+           )
+
+          // TEST
+          tables.buyers.map(b =>
+            tables.shipInfos.flatMap(si =>
+              (name = b.name, shippingDate = si.shippingDate).toRow
+            )
+          )
+          """)
+    assert(error.contains(expectedError), s"Expected substring '$expectedError' in '$error'")
+  }
+}
+
+class AggregateCompileErrorTest extends munit.FunSuite {
+  def testDescription: String = "aggregate that returns scalar expr should fail"
+  def expectedError: String = "None of the overloaded alternatives of method aggregate" // TODO: can we force a better error message?
+
+  test(testDescription) {
+    val error: String =
+      compileErrors(
+        """
+           // BOILERPLATE
+           import language.experimental.namedTuples
+           import tyql.{Table, Expr}
+           import java.time.LocalDate
+
+           case class Product(id: Int, name: String, price: Double)
+
+           case class Buyer(id: Int, name: String, dateOfBirth: LocalDate)
+
+           case class ShippingInfo(id: Int, buyerId: Int, shippingDate: LocalDate)
+
+           case class Purchase(
+                     id: Int,
+                     shippingInfoId: Int,
+                     productId: Int,
+                     count: Int,
+                     total: Double
+                   )
+
+           val tables = (
+             products = Table[Product]("product"),
+             buyers = Table[Buyer]("buyers"),
+             shipInfos = Table[ShippingInfo]("shippingInfo"),
+             purchases = Table[Purchase]("purchase")
+           )
+
+          // TEST
+          tables.products
+            .aggregate(p => p.price)
+          """)
+    assert(error.contains(expectedError), s"Expected substring '$expectedError' in '$error'")
+  }
+}
+
+class AggregateSubqueryCompileErrorTest extends munit.FunSuite {
+  def testDescription: String = "aggregate with aggregate subquery as source should fail"
+  def expectedError: String = "map is not a member of tyql.Aggregation"
+
+  test(testDescription) {
+    val error: String =
+      compileErrors(
+        """
+           // BOILERPLATE
+           import language.experimental.namedTuples
+           import tyql.{Table, Expr}
+           import java.time.LocalDate
+
+           case class Product(id: Int, name: String, price: Double)
+
+           case class Buyer(id: Int, name: String, dateOfBirth: LocalDate)
+
+           case class ShippingInfo(id: Int, buyerId: Int, shippingDate: LocalDate)
+
+           case class Purchase(
+                     id: Int,
+                     shippingInfoId: Int,
+                     productId: Int,
+                     count: Int,
+                     total: Double
+                   )
+
+           val tables = (
+             products = Table[Product]("product"),
+             buyers = Table[Buyer]("buyers"),
+             shipInfos = Table[ShippingInfo]("shippingInfo"),
+             purchases = Table[Purchase]("purchase")
+           )
+
+          // TEST
+          tables.products.aggregate(p => (avgPrice = Expr.avg(p.price))).map(r => r == 10)
+          """)
+    assert(error.contains(expectedError), s"Expected substring '$expectedError' in '$error'")
+  }
+}
