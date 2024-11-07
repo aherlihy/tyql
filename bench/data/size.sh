228d1242bc099d17baab10379e2767a09fd75062
aherlihy <herlihyap@gmail.com>
2024-11-07 11:13:50 +0100

    Update sizes script

--- bench/data/size.sh
+++ bench/data/size.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
 # Set the directory name variable
-datadir="data_1MB"  # You can change this to any directory name you want
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Usage: $0 <directory_name> <unit>"
+    echo "Example: $0 data_1MB MB"
+    echo "Example: $0 data_1MB B"
+    exit 1
+fi
+
+datadir="$1"
+unit="$2"
+
+# Validate the unit argument
+if [[ "$unit" != "MB" && "$unit" != "B" ]]; then
+    echo "Invalid unit. Please specify 'MB' or 'B'."
+    exit 1
+fi
 
 # Loop through each '$datadir' directory in the current directory structure
 for dir in */"$datadir"; do
@@ -11,7 +25,12 @@ for dir in */"$datadir"; do
     parent_dir=$(basename "$(dirname "$dir")")
 
     # Calculate the size of the '$datadir' directory in MB
-    total_size=$(du -s "$dir" | awk '{print int($1/1024)" MB"}')
+  if [ "$unit" == "MB" ]; then
+      total_size=$(du -sk "$dir" | awk '{printf "%.2f MB", $1/1024}')
+  else
+      total_size=$(du -sk "$dir" | awk '{printf "%d B", $1 * 1024}')
+  fi
+
     echo "$parent_dir/$datadir: ${total_size}"
 
     # Loop through each CSV file in the '$datadir' directory
@@ -20,7 +39,11 @@ for dir in */"$datadir"; do
       if [ -f "$file" ]; then
         # Get the file name and its size in MB
         file_name=$(basename "$file")
-        file_size=$(du -s "$file" | awk '{print int($1/1024)" MB"}')
+        if [ "$unit" == "MB" ]; then
+            file_size=$(du -sk "$file" | awk '{printf "%.2f MB", $1/1024}')
+        else
+            file_size=$(du -sk "$file" | awk '{printf "%d B", $1 * 1024}')
+        fi
         echo -e "\t$parent_dir/$file_name: ${file_size}"
       fi
     done
