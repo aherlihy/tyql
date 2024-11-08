c134b183acf89f418b117b4757a5ad81d940b2f1
aherlihy <herlihyap@gmail.com>
2024-11-08 08:50:17 +0100

    revert using delta for termination check

--- bench/FixedPoint.scala
+++ bench/FixedPoint.scala
@@ -13,11 +13,10 @@ object FixedPointQuery {
   final def fix[P](set: Boolean)(bases: Seq[P], acc: Seq[P])(fns: Seq[P] => Seq[P]): Seq[P] =
     if (Thread.currentThread().isInterrupted) throw new Exception(s"timed out")
     val next = fns(bases)
-    val nextAcc = (acc ++ bases)
-    if (next.toSet.subsetOf(nextAcc.toSet))
-      if (set) then nextAcc.distinct else nextAcc
+    if (next.toSet.subsetOf(acc.toSet))
+      if (set) then (acc ++ bases).distinct else acc ++ bases
     else
-      val res = if (set) then nextAcc.distinct else nextAcc
+      val res = if (set) then (acc ++ bases).distinct else acc ++ bases
       val nextClean = next.filterNot(n => res.contains(n))
       fix(set)(nextClean, res)(fns)
 
@@ -30,18 +29,18 @@ object FixedPointQuery {
     val basesA = bases.toList.asInstanceOf[List[S]]
     val accA = acc.toList.asInstanceOf[List[S]]
 
-    val nextAcc = accA.zip(basesA).map((a: S, b: S) => if (set) then (a ++ b).distinct else a ++ b)
 
-
-    val cmp = nextA.zip(nextAcc).map(
+    val cmp = nextA.zip(accA).map(
       (n, a) => n.toSet.subsetOf(a.toSet)
     ).forall(b => b)
     if (cmp)
-      val res = Tuple.fromArray(nextAcc.toArray).asInstanceOf[T]
+      val combo = accA.zip(basesA).map((a: S, b: S) => if (set) then (a ++ b).distinct else a ++ b)
+      val res = Tuple.fromArray(combo.toArray).asInstanceOf[T]
       res
     else
-      val newAcc = Tuple.fromArray(nextAcc.toArray).asInstanceOf[T]
-      val nextClean = nextA.zip(nextAcc).map((n, r) => n.filterNot(n => r.contains(n)))
+      val combo = accA.zip(basesA).map((a: S, b: S) => if (set) then (a ++ b).distinct else a ++ b)
+      val newAcc = Tuple.fromArray(combo.toArray).asInstanceOf[T]
+      val nextClean = nextA.zip(combo).map((n, r) => n.filterNot(n => r.contains(n)))
       val newNext = Tuple.fromArray(nextClean.toArray).asInstanceOf[T]
       multiFix(set)(newNext, newAcc)(fns)
 
