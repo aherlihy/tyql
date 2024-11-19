5779dee8138d18bf5b096bba63a2c83538a32643
Bartosz Białas <bartosz.bialas@epfl.ch>
2024-11-19 20:14:14 +0100

    test: toQueryIR, toSQLString caching

--- src/test/scala/test/dialects/queries cached correctly between dialects and configs.scala
+++ src/test/scala/test/dialects/queries cached correctly between dialects and configs.scala
@@ -1,25 +1,50 @@
 package test.dialects.cachingworksbetweendialectsandconfigs
 
 import munit.FunSuite
+import tyql.Dialect
 
 class QueryCachingWorksBetweenDialectsAndConfigs extends FunSuite {
 
   case class Row(i: Int)
   val t = tyql.Table[Row]("t")
 
-  // TODO this is currently broken
-  test("query caching works between dialects and configs".ignore) {
-    var q: tyql.QueryIRNode = null
-    {
-      q = t.map(_ => tyql.Expr.StringLit("abc").charLength).toQueryIR
-    }
+  test("query caching between dialects") {
+    // TODO XXX toQueryIR is dialect-dependent!!! Is this OK or not?
+    // XXX by changing the number of calls and looking at printouts in the logs you can see that caching works
+    var q: tyql.Query[?, ?] = null
     {
       import tyql.Dialect.postgresql.given
-      println(expr.toSQLString())
+      q = t.map(_ => tyql.Expr.StringLit("abc").charLength)
+      val ir = q.toQueryIR
+      val ir2 = q.toQueryIR
+      val ir3 = q.toQueryIR
+      val s1 = ir.toSQLString()
+      val s2 = ir.toSQLString()
+      val s3 = ir.toSQLString()
+      val s4 = ir.toSQLString()
+      val s5 = ir.toSQLString()
+      assertEquals(s1, s2)
+      assertEquals(s1, s3)
+      assertEquals(s1, s4)
+      assertEquals(s1, s5)
+      assert(s1.toLowerCase().contains("length("), s1)
     }
     {
       import tyql.Dialect.mysql.given
-      println(expr.toSQLString())
+      // q = t.map(_ => tyql.Expr.StringLit("abc").charLength).toQueryIR // we do not regenerate
+      val ir = q.toQueryIR
+      val ir2 = q.toQueryIR
+      val ir3 = q.toQueryIR
+      val s1 = ir.toSQLString()
+      val s2 = ir.toSQLString()
+      val s3 = ir.toSQLString()
+      val s4 = ir.toSQLString()
+      val s5 = ir.toSQLString()
+      assertEquals(s1, s2)
+      assertEquals(s1, s3)
+      assertEquals(s1, s4)
+      assertEquals(s1, s5)
+      assert(s1.toLowerCase().contains("char_length("), s1)
     }
   }
 }
