f46a34a39cc33f94afcfbe881314d386754b3120
Bartosz Białas <bartosz.bialas@epfl.ch>
2024-12-29 17:09:21 +0100

    do not use deprecated implicits

--- src/main/scala/tyql/query/Aggregation.scala
+++ src/main/scala/tyql/query/Aggregation.scala
@@ -55,7 +55,7 @@ object Aggregation:
 
       val groupResult = groupingFn(refsTuple)
 
-      Query.NewGroupBy(this, groupResult, argRefs, sourceTags, None)
+      Query.NewGroupBy(this, groupResult, argRefs.toIndexedSeq, sourceTags, None)
 
   case class AggFilter[A: ResultTag]($from: Query[A, ?], $pred: Expr.Pred[A, ScalarExpr]) extends Aggregation[A *: EmptyTuple, A]:
     def groupBySource[GroupResult, GroupShape <: ExprShape]
